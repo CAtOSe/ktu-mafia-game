561828685f170bbdca22b5a963485d52089a103b
CAtOSe <jasenasp@gmail.com>
2024-11-02 10:47:13 +0200

    Fix things

--- mafia-ui/src/components/MainGame/MainGame.tsx
+++ mafia-ui/src/components/MainGame/MainGame.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import DayTracker from './DayTracker/DayTracker.tsx';
 import RoleInformation from './RoleInformation/RoleInformation.tsx';
-import RoleList from './RoleList/RoleList.tsx';
 import Graveyard from './Graveyard/Graveyard.tsx';
 import AlivePlayersList from './AlivePlayersList/AlivePlayersList.tsx';
 import Chatbox from './Chatbox/Chatbox.tsx';
@@ -13,9 +12,6 @@ const cn = classNames.bind(styles);
 const MainGame: React.FC = () => {
   return (
     <div className={cn('main-game')}>
-      <div className={cn('role-list')}>
-        <RoleList roles={[]} />
-      </div>
       <div className={cn('role-list')}>
         <Graveyard />
       </div>
